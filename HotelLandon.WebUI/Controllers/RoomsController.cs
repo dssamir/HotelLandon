5206c9623710e8f5305ac4a2b98c52f9bdbb77df
Samir DJILALI SAIAH <ds_samir1@hotmail.com>
2019-06-04 11:27:33 +0200

    update

---
+++ HotelLandon.WebUI/Controllers/RoomsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelLandon.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelLandon.WebUI.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly HotelLandonContext _context;
+        public RoomsController(HotelLandonContext context)
+            => _context = context;
+        public IActionResult Index()
+        {
+            return View(_context.Rooms.ToList());
+        }
+    }
+}

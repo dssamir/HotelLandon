620d9d69b799eee2879b2fddcbcdc9e8b5cbdf29
Samir DJILALI SAIAH <ds_samir1@hotmail.com>
2019-06-04 15:46:07 +0200

    gulp, minify, default

--- HotelLandon.WebUI/gulpfile.js
+++ HotelLandon.WebUI/gulpfile.js
@@ -1,9 +1,18 @@
 ﻿'use strict'
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
 
-gulp.task("gulp-sass", function () {
+gulp.task("sass", function () {
     return gulp.src("wwwroot/scss/site.scss")
         .pipe(sass())
         .pipe(gulp.dest("wwwroot/css/"));
 });
+gulp.task("minify-css", function () {
+    return gulp.src(["wwwroot/css/*.css", "!wwwroot/css/*.min.css"])
+        .pipe(rename({ extname: "min.css" }))
+        .pipe(csso())
+        .pipe(gulp.dest("wwwroot/css"));
+});
+exports.default = gulp.series(["sass", "minify-css"]);

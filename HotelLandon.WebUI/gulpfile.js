073bc07a101adc4fe65a72b14637654e81d2b1b6
Samir DJILALI SAIAH <ds_samir1@hotmail.com>
2019-06-04 15:25:35 +0200

    gulp

---
+++ HotelLandon.WebUI/gulpfile.js
@@ -0,0 +1,9 @@
+﻿'use strict'
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("gulp-sass", function () {
+    return gulp.src("wwwroot/scss/site.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("wwwroot/css/"));
+});
